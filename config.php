c925ae5faa31ee9904bd53d263a0b5958c6e6d3a
Shweta Srivastava <64269840+Shweta490@users.noreply.github.com>
2021-10-04 22:37:28 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-		$conn = mysqli_connect('localhost','id15658857_akansha_vatsa','Akanshavatsa@123','id15658857_foundation_bank');
+		$conn = mysqli_connect('localhost','id15658857_shweta_srivastava','shwetasrivastava738@','id15658857_foundation_bank');
 
 	if(!$conn){
 		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
